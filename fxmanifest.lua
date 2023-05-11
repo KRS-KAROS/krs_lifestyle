fdd0d5a8437b48adb8018aed2dcc4fb2b91926a9
KRS <131356071+KRS-KAROS@users.noreply.github.com>
2023-05-11 17:15:05 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+Nome 'krs_lifestyle'
+Autore '𝗞𝗥𝗦®'
+Discord 'https://discord.gg/wM4XDaXfU8' -- 𝗞𝗥𝗦® --
+
+client_script {
+    'client/**.lua'
+}
+
+server_script {
+    '@oxmysql/lib/MySQL.lua',
+    'server/**.lua'
+}
+
+ui_page 'ui/**.html'
+
+files{
+    'ui/**.html',
+    'ui/**.css',
+    'ui/**.js',
+    'img/**.png',
+    'img/**.png',
+    'img/**.png',
+    'img/**.png',
+}
---
+++ ui/karos.js
@@ -0,0 +1,101 @@
+const playerScelta = document.querySelector("#scelta");
+
+function openLife() {
+  document.querySelector(".background").style.display = "block";
+  document.querySelector(".title").style.display = "block";
+  document.querySelector(".boxkaros").style.display = "block";
+  document.querySelector(".boxkaros2").style.display = "block";
+  document.querySelector(".boxkaros3").style.display = "block";
+  document.querySelector(".boxkaros4").style.display = "block";
+  
+
+}
+
+window.addEventListener("message", function (event) {
+  let data = event.data;
+  if (data.type === "openLife") {
+    openLife();
+
+  }
+});
+
+function post(path) {
+  fetch(path, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({}),
+  }).then((resp) => resp.json());
+}
+
+function selectCivil() {
+    post(`https://krs_lifestyle/civil`)
+    document.querySelector(".title").style.display = "none";
+    document.querySelector(".boxkaros").style.display = "none";
+    document.querySelector(".boxkaros2").style.display = "none";
+    document.querySelector(".boxkaros3").style.display = "none";
+    document.querySelector(".boxkaros4").style.display = "none";
+    document.querySelector(".background").style.display = "none";
+}
+function selectGangster() {
+    post(`https://krs_lifestyle/gangster`)
+    document.querySelector(".title").style.display = "none";
+    document.querySelector(".boxkaros").style.display = "none";
+    document.querySelector(".boxkaros2").style.display = "none";
+    document.querySelector(".boxkaros3").style.display = "none";
+    document.querySelector(".boxkaros4").style.display = "none";
+    document.querySelector(".background").style.display = "none";
+}
+function selectDrifter() {
+    post(`https://krs_lifestyle/drifter`)
+    document.querySelector(".title").style.display = "none";
+    document.querySelector(".boxkaros").style.display = "none";
+    document.querySelector(".boxkaros2").style.display = "none";
+    document.querySelector(".boxkaros3").style.display = "none";
+    document.querySelector(".boxkaros4").style.display = "none";
+    document.querySelector(".background").style.display = "none";
+}
+function selectMafia() {
+    post(`https://krs_lifestyle/mafia`)
+    document.querySelector(".title").style.display = "none";
+    document.querySelector(".boxkaros").style.display = "none";
+    document.querySelector(".boxkaros2").style.display = "none";
+    document.querySelector(".boxkaros3").style.display = "none";
+    document.querySelector(".boxkaros4").style.display = "none";
+    document.querySelector(".background").style.display = "none";
+}
+
+function selectEntrepreneur() {
+    post(`https://krs_lifestyle/entrepreneur`)
+    document.querySelector(".title").style.display = "none";
+    document.querySelector(".boxkaros").style.display = "none";
+    document.querySelector(".boxkaros2").style.display = "none";
+    document.querySelector(".boxkaros3").style.display = "none";
+    document.querySelector(".boxkaros4").style.display = "none";
+    document.querySelector(".background").style.display = "none";
+}
+
+
+
+//Funzione Close
+document.onkeydown = function (evt) {
+  evt = evt || window.event;
+  var isEscape = false;
+  if ("key" in evt) {
+    isEscape = evt.key === "Escape" || evt.key === "Esc";
+  } else {
+    isEscape = evt.keyCode === 27;
+  }
+  if (isEscape) {
+    
+    post(`https://${GetParentResourceName()}/close`);
+      
+    document.querySelector(".title").style.display = "none";
+    document.querySelector(".boxkaros").style.display = "none";
+    document.querySelector(".boxkaros2").style.display = "none";
+    document.querySelector(".boxkaros3").style.display = "none";
+    document.querySelector(".boxkaros4").style.display = "none";
+    document.querySelector(".background").style.display = "none";
+  }
+};
